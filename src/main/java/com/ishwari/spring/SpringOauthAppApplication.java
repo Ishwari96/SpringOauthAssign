f9d3015ca3a0db9b3dbedf14605fbbdbebbd5069
ishwari96 <ishwaripdoshi@gmail.com>
2021-07-10 19:45:27 +0200

    Added Oauth project setup

---
+++ src/main/java/com/ishwari/spring/SpringOauthAppApplication.java
@@ -0,0 +1,13 @@
+package com.ishwari.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishwari/spring/SpringOauthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishwari.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
